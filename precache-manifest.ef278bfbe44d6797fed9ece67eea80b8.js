d23a4638e1fe9e45b3b34b595945390d3860e6b9
andrewfaulkner <andrewfau1kn3r@gmail.com>
2021-11-01 16:40:25 +0800

    Updates

---
+++ precache-manifest.ef278bfbe44d6797fed9ece67eea80b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df32329510e8bb94eb18eeffc52f9ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb012ad495822ef451ed",
+    "url": "./static/css/main.f0c9ae93.chunk.css"
+  },
+  {
+    "revision": "a46c8e32dc8767ea7187",
+    "url": "./static/js/2.e4754906.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "./static/js/2.e4754906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb012ad495822ef451ed",
+    "url": "./static/js/main.355f9ff3.chunk.js"
+  },
+  {
+    "revision": "73342004b909d7ce2f55",
+    "url": "./static/js/runtime-main.6efa65b6.js"
+  }
+]);
